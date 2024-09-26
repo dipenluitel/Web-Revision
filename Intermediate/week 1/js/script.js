23a7f3385e5c9dad560fc71a56a160bc9f0241fd
Dipen Luitel <46955586+dipenluitel@users.noreply.github.com>
2024-09-26 16:17:57 +0545

    Create script.js

---
+++ Intermediate/week 1/js/script.js
@@ -0,0 +1,7 @@
+// script.js
+const menuToggle = document.querySelector('.menu-toggle');
+const navLinks = document.querySelector('.nav-links');
+
+menuToggle.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+});
